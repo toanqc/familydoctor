18da3b7da34b4a0bd8135d8c89947c8f2cc09217
Toan Quach <toanqc@gmail.com>
2015-07-30 18:24:24 -0500

    add .gitignore

--- src/main/java/mum/waa/fd/app/controller/MainController.java
+++ src/main/java/mum/waa/fd/app/controller/MainController.java
@@ -1,13 +1,8 @@
 package mum.waa.fd.app.controller;
 
-//import mum.waa.fd.service.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
