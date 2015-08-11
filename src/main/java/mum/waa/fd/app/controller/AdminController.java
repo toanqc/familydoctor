fc9b30ce56f8c202245786ca4364d0ecc73160e2
984703 <984703@CS_LAB218-207.cs.mum.edu>
2015-08-11 12:23:26 -0500

    admin

--- src/main/java/mum/waa/fd/app/controller/AdminController.java
+++ src/main/java/mum/waa/fd/app/controller/AdminController.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +15,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 
 import mum.waa.fd.app.service.DoctorService;
+import mum.waa.fd.app.validator.PasswordValidator;
 import mum.waa.fd.app.domain.Doctor;
+import mum.waa.fd.app.domain.Specialization;
 
 @Controller
 public class AdminController {
@@ -34,7 +36,7 @@ public class AdminController {
 	    SimpleDateFormat dateOfBirth = new SimpleDateFormat("MM/dd/yyyy");
 	    dateOfBirth.setLenient(true);
 	    binder.registerCustomEditor(Date.class, new CustomDateEditor(dateOfBirth, true));
-	}	
+	}
 	
 	@RequestMapping("/admin")
 	public String adminDashboard() {
@@ -44,17 +46,22 @@ public class AdminController {
 	@RequestMapping(value = "/admin/add-doctor", method = RequestMethod.GET)
 	public String addDoctorAcount(@ModelAttribute("newDoctor") Doctor newDoctor, Model model) {
 		
+		model.addAttribute("specializations", Specialization.values());
+		
 		return "admin-add-doctor";
 	}
 
 	@RequestMapping(value = "/admin/save-doctor", method = RequestMethod.POST)
-	public String saveDoctor(@ModelAttribute("newDoctor") @Valid Doctor newDoctor, Model model, BindingResult result) {
-		if( ! result.hasErrors() ){
-			doctorService.saveDoctor(newDoctor);
-		} else {
-			System.out.println("Invalid");
+	public String saveDoctor(@ModelAttribute("newDoctor") @Valid Doctor newDoctor, Model model, 
+		BindingResult result, RedirectAttributes redirectAttributes) {
+
+		if( result.hasErrors() ){
+			return "admin/add-doctor";
 		}
 		
+		doctorService.saveDoctor(newDoctor);
+		
+		redirectAttributes.addFlashAttribute("message", "Doctor successfully added");	
 		return "redirect:/admin/add-doctor";
 	}
 
